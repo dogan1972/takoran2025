5c8ee65fe880312919f1ac8e445668c91d75d6f9
Doğan KANLIPIÇAK <dogan1972@gmail.com>
2025-01-21 11:48:25 +0300

    key bilgisi key dosyasında

--- key_sifre.dart
+++ key_sifre.dart
@@ -1,1 +1,4 @@
-//Şifre IHLIHL
+// Birinci adım
+//keytool -genkey -v -keystore c:\Users\user001\takoran_key.jks -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias flutter
+//ikinci adım
+//Enter keystore password: IHLIHL
